d8fdb968e07a485cdaa70005afa93b4e05fd10e6
Sarani <saranipeiris17@gmail.com>
2019-08-31 10:36:28 +0530

    Added bullets

--- lib/items.dart
+++ lib/items.dart
@@ -1,28 +1,49 @@
 import 'package:flutter/material.dart';
 
 class Items extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.do_not_disturb), text: "RESTRICTED",),
-                Tab(icon: Icon(Icons.done), text: "PERMITTED"),
-              ],
-            ),
-            title: Text('LIST OF GOODS'),
-          ),
-          body: TabBarView(
-            children: [
-              Center( child: Text("Page 1")),
-              Center( child: Text("Page 2")),
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.do_not_disturb),
+                text: "RESTRICTED",
+              ),
+              Tab(icon: Icon(Icons.done), text: "PERMITTED"),
             ],
           ),
+          centerTitle: true,
+          title: Text('LIST OF GOODS'),
         ),
-      );
+        body: TabBarView(
+          children: <Widget>[
+            (new ListTile(
+              leading: new MyBullet(),
+              title: new Text('My first line'),
+            )),
+            new ListTile(
+                leading: new MyBullet(), title: new Text('My second line')),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyBullet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      height: 15.0,
+      width: 15.0,
+      decoration: new BoxDecoration(
+        color: Colors.black,
+        shape: BoxShape.rectangle,
+      ),
+    );
   }
 }
