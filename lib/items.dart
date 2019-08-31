577996ce83358b4cf3072d684a460b5a7b669573
Sarani <saranipeiris17@gmail.com>
2019-08-31 14:31:04 +0530

    permitted done

--- lib/items.dart
+++ lib/items.dart
@@ -24,9 +24,192 @@ class Items extends StatelessWidget {
             new ListView(
               children: list,
             ),
-            new ListView(
-              children: list,
-            ),
+            SingleChildScrollView(
+              child: DataTable(
+                columns: <DataColumn>[
+                  DataColumn(
+                    label: Text('Item',
+                        style: new TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 20.0,color: Colors.black,decoration:TextDecoration.underline)),
+                  ),
+                  DataColumn(
+                      label: Text('Amount',
+                          style: new TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 20.0,color: Colors.black,decoration:TextDecoration.underline))),
+                ],
+                rows: <DataRow>[
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Alarm Clock'),
+                    ),
+                    DataCell(
+                      Text('2'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Artificial Flowers'),
+                    ),
+                    DataCell(
+                      Text('12'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Baby Carriages'),
+                    ),
+                    DataCell(
+                      Text('2'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Bathing Suits/Trunks'),
+                    ),
+                    DataCell(
+                      Text('6'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Bathroom Set(With Accessories)'),
+                    ),
+                    DataCell(
+                      Text('1'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text(
+                          'Bedroom Set(One wadrobe,One dressing table and one bed'),
+                    ),
+                    DataCell(
+                      Text('1'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Bed Sheets'),
+                    ),
+                    DataCell(
+                      Text('6'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Bed Spreads'),
+                    ),
+                    DataCell(
+                      Text('6'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Beds with or without mattresses'),
+                    ),
+                    DataCell(
+                      Text('3'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Beer'),
+                    ),
+                    DataCell(
+                      Text('2l'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Belts'),
+                    ),
+                    DataCell(
+                      Text('6'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Bicylces Not Motorized'),
+                    ),
+                    DataCell(
+                      Text('2'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Blankets'),
+                    ),
+                    DataCell(
+                      Text('4'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Brief Cases'),
+                    ),
+                    DataCell(
+                      Text('2'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Carpets'),
+                    ),
+                    DataCell(
+                      Text('2'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Chandeliers'),
+                    ),
+                    DataCell(
+                      Text('2'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Dining Chairs'),
+                    ),
+                    DataCell(
+                      Text('8'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Computer Diskette'),
+                    ),
+                    DataCell(
+                      Text('6 boxes'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Hats and Caps'),
+                    ),
+                    DataCell(
+                      Text('6'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Hot water Gyser'),
+                    ),
+                    DataCell(
+                      Text('1'),
+                    ),
+                  ]),
+                  DataRow(cells: <DataCell>[
+                    DataCell(
+                      Text('Keytags'),
+                    ),
+                    DataCell(
+                      Text('12'),
+                    ),
+                  ]),
+                ],
+              ),
+            )
           ],
         ),
       ),
@@ -88,5 +271,22 @@ List<Widget> list = <Widget>[
     subtitle: new Text('OVER 55"'),
     leading: new MyBullet(),
   ),
+  new ListTile(
+    title: new Text('T.V LCD/LED/PLASMA',
+        style: new TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0)),
+    subtitle: new Text('OVER 55"'),
+    leading: new MyBullet(),
+  ),
+  new ListTile(
+    title: new Text('T.V LCD/LED/PLASMA',
+        style: new TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0)),
+    subtitle: new Text('OVER 55"'),
+    leading: new MyBullet(),
+  ),
+  new ListTile(
+    title: new Text('T.V LCD/LED/PLASMA',
+        style: new TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0)),
+    subtitle: new Text('OVER 55"'),
+    leading: new MyBullet(),
+  ),
 ];
-
