db7b3959f5ab33f07b4ee3d2cb91b09feedc807e
pyrecyter <sachiranuwanga123@gmail.com>
2019-09-01 12:11:27 +0530

    WEB VIEW added

--- lib/OurApps.dart
+++ lib/OurApps.dart
@@ -1,9 +1,19 @@
+import 'package:duty_free_allowance/ourAppsWeb.dart';
 import 'package:flutter/material.dart';
 
-class OurApps extends StatelessWidget {
+class OurApps extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return  Container(  decoration: BoxDecoration(
+  State<StatefulWidget> createState() {
+    return new Screen();
+  }
+}
+
+class Screen extends State<OurApps> {
+  int status = 0;
+
+  mainScreen() {
+    return Container(
+      decoration: BoxDecoration(
         color: Colors.blueAccent,
         image: DecorationImage(
           colorFilter: new ColorFilter.mode(
@@ -11,7 +21,9 @@ class OurApps extends StatelessWidget {
           image: AssetImage('assets/visible_tec.png'),
           fit: BoxFit.cover,
         ),
-      ),child: Column(children : [Container(
+      ),
+      child: Column(children: [
+        Container(
             padding: EdgeInsets.only(top: 200.0),
             child: new Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -32,42 +44,56 @@ class OurApps extends StatelessWidget {
                 ),
               ],
             )),
-    Container(
+        Container(
             width: MediaQuery.of(context).size.width,
             margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 150.0),
             alignment: Alignment.center,
-            child: new Row(
-              children: <Widget>[
-                new Expanded(
+            child: new Row(children: <Widget>[
+              new Expanded(
                   child: new FlatButton(
-                    shape: new RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(30.0)),
-                    color: Colors.white,
-                    onPressed: () {}, //Put allowance route here 
-                    child: new Container(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 20.0,
-                        horizontal: 20.0,
-                      ),
-                      child: new Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          new Expanded(
-                            child: Text(
-                              "MORE APPS",
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold),
-                            ),
+                      shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(30.0)),
+                      color: Colors.white,
+                      onPressed: () {
+                        setState(() {
+                          status = 1;
+                        });
+                      }, //Put allowance route here
+                      child: new Container(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 20.0,
+                            horizontal: 20.0,
                           ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),) ])
+                          child: new Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              new Expanded(
+                                child: Text(
+                                  "MORE APPS",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ],
+                          ))))
+            ])),
+      ]),
     );
+  }
+
+  _checkStat(int i) {
+    switch (i) {
+      case 1:
+        return OurAppsWeb();
+      default:
+        return mainScreen();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _checkStat(status);
   }
 }
