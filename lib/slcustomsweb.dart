2bb24fe8fa046e7be4f68e483f4f5cb2fbccce2d
Sarani <saranipeiris17@gmail.com>
2019-09-06 22:39:17 +0530

    SLCustomsWEb completed

--- lib/slcustomsweb.dart
+++ lib/slcustomsweb.dart
@@ -1,2 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
+class SlCustomsWeb extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    WebViewController _controller;
+    return WebView(
+        initialUrl: 'http://www.customs.gov.lk/traveller/home',
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller = webViewController;
+        });
+  }
+}
