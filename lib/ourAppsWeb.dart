464dc3d56a2a6ee6ff4c102a26d938488d242a60
Isuru Liyanage <isuruliyanage1@gmail.com>
2019-09-15 12:20:37 +0530

    our web appbar fixed

--- lib/ourAppsWeb.dart
+++ lib/ourAppsWeb.dart
@@ -5,10 +5,14 @@ class OurAppsWeb extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     WebViewController _controller;
-    return WebView(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Our Web'),
+      ),
+      body:WebView(
         initialUrl: 'https://www.visibletec.com',
         onWebViewCreated: (WebViewController webViewController) {
           _controller = webViewController;
-        });
+        }));
   }
 }
