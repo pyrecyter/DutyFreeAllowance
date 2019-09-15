8bacb131076e2cdbcf7dcdab2ad22b079b0d7a0b
Isuru Liyanage <isuruliyanage1@gmail.com>
2019-09-15 12:54:06 +0530

    Allowance button fixed :)

--- lib/routing_constant.dart
+++ lib/routing_constant.dart
@@ -2,3 +2,4 @@ const String HomeViewRoute = '/';
 const String AllowanceViewRoute = 'Allowance';  
 const String OurwebViewRoute = 'ourAppsWeb'; 
 const String AppViewRoute = 'moreapps';
+const String ItemRoute  = 'items';
